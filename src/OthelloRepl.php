6b4b4ea0e2603ad934d340ec95bbd2b74fb861a5
coffeecup <IgYmB0218LP@gmail.com>
2022-12-05 16:14:46 +0900

    impl computer play

--- src/OthelloRepl.php
+++ src/OthelloRepl.php
@@ -51,7 +51,53 @@ class OthelloRepl {
 		$this->othello->getGameResult();
 	}
 
+	public function play_with_computer() {
+		$choose_player_flag = true;
+		$chosen_player = 1;
+		while($choose_player_flag == true) {
+			echo "Please Input Player(1 for black 2 for white);\n";
+			$stdin_player = (int)trim(fgets(STDIN));
+			if ($stdin_player == 1) {
+				$choose_player_flag = false;
+				$chosen_player = 1;
+			} else if ($stdin_player == 2) {
+				$chosen_player = false;
+				$chosen_player = 2;
+			} else {
+				echo "Please Input correct input (1 or 2).\n";
+			}
+		}
+		while ($this->isGameFinished == false) {
+			[$display_board, $candidate_moves] = $this->othello->getCandidateBoard();
+			echo Viewer::view_board($display_board)."\n";
+			if ($this->othello->getPlayer() == $chosen_player) {
+				echo "It's your turn ".$this->othello->getPlayer()." \n";
+				echo "Please Input vertical position;\n";
+				$stdin_vertical = (int)trim(fgets(STDIN));
+				echo "Please Input horizontal position;\n";
+				$stdin_horizontal = (int)trim(fgets(STDIN));
+				[$is_success, $is_game_continue] = $this->othello->move($stdin_vertical - 1, $stdin_horizontal - 1);
+				if ($is_game_continue == false ) {
+					$this->isGameFinished = false;
+				}
+				if ($is_success == true) {
+					echo "\n";
+				} else {
+					echo "Your move is incorrect...\nmove again\n\n";
+				}
+			} else {
+				[$is_success, $is_game_continue] = $this->othello->random_move();
+				echo "Computer played.\n\n";
+				if ($is_game_continue == false ) {
+					$this->isGameFinished = true;
+				}
+			}
+		}
+		$this->othello->getGameResult();
+	}
+
 }
 
 $repl = new OthelloRepl();
-$repl->random_game();
+// $repl->random_game();
+$repl->play_with_computer();
